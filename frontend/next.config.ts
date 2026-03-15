6b726b81f499d9c2924792cf7549a4e5db1eef86
Suyog Sonawane <44076436+suyogs1@users.noreply.github.com>
2026-03-15 19:35:40 +0530

    Update WebSocket URL in next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   env: {
-    NEXT_PUBLIC_WS_URL: "wss://chrono-lens-backend-sknvuepfjq-uc.a.run.app"
+    NEXT_PUBLIC_WS_URL: "wss://chrono-lens-backend-434492372587.us-central1.run.app/ws/live"
   }
   /* config options here */
 };
